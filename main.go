2a1c24b5c77ddcddc3d9c679b7a65f63a3f1be1b
4Noyis <berenalpb@outlook.com>
2024-11-17 02:50:21 +0200

    test

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/4Noyis/cli-todo-app/cmd/app"
+import (
+	"fmt"
+
+	"github.com/4Noyis/cli-todo-app/cmd/app"
+)
 
 func main() {
+	fmt.Printf("sea")
 	app.Run()
 }
